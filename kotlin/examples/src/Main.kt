9cd9d92f5802326bd2553d291323f8abbfdcc2ae
박성흠 <jake@Bladek-Air.local>
2017-10-12 23:11:18 +0900

    [Add] 제네릭 관련 예제 코드 추가

--- kotlin/examples/src/Main.kt
+++ kotlin/examples/src/Main.kt
@@ -22,4 +22,28 @@ class Main {
         }
         println(person)
     }
+
+    private fun <T> copy(dest: MutableList<T>, src: List<T>) {
+        for (item: T in src) {
+            dest.add(item)
+        }
+    }
+
+    val FILL_THRESHOLD = 10
+
+    private fun <T> fill(list: MutableList<in T>, obj: T) {
+        val size = list.size
+
+        if (size < FILL_THRESHOLD || list is RandomAccess) {
+            for (i in 0 until size) {
+                list[i] = obj
+            }
+        } else {
+            val itr = list.listIterator()
+            for (i in 0 until size) {
+                itr.next()
+                itr.set(obj)
+            }
+        }
+    }
 }
